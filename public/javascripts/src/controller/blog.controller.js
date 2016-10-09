5c8f801a7d78a7b70d1723712b1ef4f85102a3ba
coolcao <me@coolcao.com>
2016-10-09 23:08:42 +0800

    博客列表分页

--- public/javascripts/src/controller/blog.controller.js
+++ public/javascripts/src/controller/blog.controller.js
@@ -5,13 +5,29 @@
 
 app.controller('BlogCtrl',['$scope','$http',function ($scope,$http) {
 
-    $scope.list = function () {
-        $http.get('/blogs').then(function (result) {
-            console.log(result.data.blogs);
+    $scope.list = function (page) {
+        page = page || 1;
+        $http.get('/blogs?page='+page).then(function (result) {
             $scope.blogs = result.data && result.data.blogs;
+            $scope.pagination = result.data && result.data.pagination;
         },function (err) {
             console.log(err);
         });
+    }
+
+    $scope.prePage = function(){
+        if($scope.pagination.hasPrePage){
+            var page = $scope.pagination.prePage;
+            $scope.list(page);
+        }
+    }
+
+    $scope.nextPage = function(){
+        if($scope.pagination.hasNextPage){
+            var page = $scope.pagination.nextPage;
+            $scope.list(page);
+        }
+        
     }
 
 }]);
